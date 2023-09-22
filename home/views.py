23bab139e23962b167bf23a818f5a456fc3982d9
Harsh12chauhan <harshchauhan0225@gmail.com>
2023-09-22 13:52:32 +0530

    created a api point in views.py using APIViews

--- home/views.py
+++ home/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+# manually added ======================
+from rest_framework.views import APIView
+from rest_framework.views import Response
+
+from .serializer import serializer
+
+from .models import userDetails
+
 
 # Create your views here.
+class userLists(APIView):
+    def get(self,request):
+        queryset = userDetails.objects.all()
+        serializersData = serializer(queryset,many=True) 
+        return Response(serializersData.data)
