d9ac41849c5d5ab2a632d3496fb48010d3682cbd
Harsh12chauhan <harshchauhan0225@gmail.com>
2023-09-22 13:55:38 +0530

    added the home/ path to urls.py inside main folder

--- main/urls.py
+++ main/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#importing userLists
+from home.views import userLists
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',userLists.as_view()),
 ]
