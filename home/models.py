cec500411957115eb20acf48701b3b3945290ebe
Harsh12chauhan <harshchauhan0225@gmail.com>
2023-09-22 12:54:54 +0530

    created model with some fields

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class userDetails(models.Model):
+    name = models.CharField(max_length=150,null=True,blank=True)
+    email = models.EmailField(null=True,blank=True)
+    password = models.CharField(max_length=200,null=True,blank=True)
+
+    def __str__(self):
+        return self.name
